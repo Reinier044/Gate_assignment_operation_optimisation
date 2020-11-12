744062c7da3d2fb92be23a80d7a14f94246aed24
Reinier044 <44966439+Reinier044@users.noreply.github.com>
2020-11-12 09:29:25 +0100

    Update Problem_formulation.py
    Changed to numpy

--- Problem_formulation.py
+++ Problem_formulation.py
@@ -4,8 +4,10 @@ Created on Thu Nov 12 09:11:23 2020
 
 @author: Reinier
 """
+import numpy as np
 
-Flights = [1,2,3,4]
-Flights_arrival = [10,10.5,11,11.5]
-Flights_t_stay = [1,1,1,1]
-Flights_PAX = [100,200,100,200]
+Flights = np.array([1,2,3,4])
+Flights_arrival = np.array([10,10.5,11,11.5])
+Flights_t_stay = np.array([1,1,1,1])
+Flights_PAX = np.array([100,200,100,200])
+
