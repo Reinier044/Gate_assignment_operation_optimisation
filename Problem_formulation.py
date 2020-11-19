7930cb9ff51ef31bd8293a52503abbdcc13505f3
Reinier044 <44966439+Reinier044@users.noreply.github.com>
2020-11-19 09:18:48 +0100

    mini_dataset creation
    Exported dataset from Problem_forumlation to mini_dataset

--- Problem_formulation.py
+++ Problem_formulation.py
@@ -9,21 +9,15 @@ import math
 import sys
 import copy
 
-#i = flight number
-#j = gate number
-
-#define flights
-Flights = np.array([1,2,3,4])
-Flights_arrival = np.array([10,10,10,10])
-Flights_class = np.array([4,3,4,4])
-Flights_t_stay = np.array([1,1,1,1])
-Flights_max_tow = np.array([2,2,2,2])
-Flights_PAX = np.array([100,200,300,400,])
+from mini_dataset import Flights,Flights_arrival,Flights_class,Flights_t_stay,Flights_max_tow,Flights_PAX
 
 #define gates
 Gates = np.array([1,2,3,4,5,6])
 Gates_class = np.array([3,4,4,4,4,4])
 Gates_distance = np.array([1000,2000,3000,4000,5000,6000])
+#i = flight number
+#j = gate number
+
 
 #define storage lists
 variables = []
---
+++ mini_dataset.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 19 09:05:28 2020
+
+@author: Reini
+"""
+import numpy as np
+#define flights
+Flights = np.array([1,2,3,4])
+Flights_arrival = np.array([10,10,10,10])
+Flights_class = np.array([4,3,4,4])
+Flights_t_stay = np.array([1,1,1,1])
+Flights_max_tow = np.array([2,2,2,2])
+Flights_PAX = np.array([100,200,300,400,])
+
+#define gates
+Gates = np.array([1,2,3,4,5,6])
+Gates_class = np.array([3,4,4,4,4,4])
+Gates_distance = np.array([1000,2000,3000,4000,5000,6000])
